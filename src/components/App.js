8c93a20306e6db375bb5b39d46c7a0b21d029720
Christopher Valle <chrisbv@live.com>
2021-11-03 11:48:42 -0500

    commit tree f96e0a2e65bfab64a85473ed5d064066962fe473

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,30 @@ import React from "react";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
+import Links from "./Links";
 
 // pass this data down as props to the child component(s) that need it!
 import user from "../data/user";
-console.log(user);
+// const user = {
+//   name: "Liza",
+//   city: "New York",
+//   bio: "I made this!",
+//   color: "firebrick",
+//   links: {
+//     github: "https://github.com/liza",
+//     linkedin: "https://www.linkedin.com/in/liza/",
+//   },
+// };
+
+// console.log(user);
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home color={user.color} username={user.name} city={user.city}/>
+      <About bio={user.bio} links={user.links}/>
+      {/* <Links Github={user.links.github} Linkedin={user.links.linkedin}/> */}
     </div>
   );
 }
---
+++ src/components/Links.js
@@ -0,0 +1,14 @@
+function Links({github, linkedin}){
+    // console.log(props)
+    // console.log(github);
+    return(
+    <>
+        <h3>Links</h3>
+        <a href={github}>{github}</a>
+        <a href={linkedin}>{linkedin}</a>
+     </>
+    )
+}
+
+
+export default Links;
